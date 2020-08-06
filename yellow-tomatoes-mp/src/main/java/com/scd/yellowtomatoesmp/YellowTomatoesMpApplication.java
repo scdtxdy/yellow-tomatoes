ceaed2fa034e6195b9da5ef942cedd76d89d7f3d
pupilman <542846492@qq.com>
2020-08-06 17:52:44 +0800

    Initial commit

---
+++ yellow-tomatoes-mp/src/main/java/com/scd/yellowtomatoesmp/YellowTomatoesMpApplication.java
@@ -0,0 +1,13 @@
+package com.scd.yellowtomatoesmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowTomatoesMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YellowTomatoesMpApplication.class, args);
+    }
+
+}
---
+++ yellow-tomatoes-system/src/test/java/com/scd/yellowtomatoessystem/YellowTomatoesSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.scd.yellowtomatoessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowTomatoesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("aa");
+    }
+
+}
