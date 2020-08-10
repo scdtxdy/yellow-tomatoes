0b78797eccdcdda89e9c868a1e740e480e0cee0f
pupilman <542846492@qq.com>
2020-08-10 17:25:01 +0800

    打印日志

---
+++ yellow-tomatoes-mproot/src/main/java/config/MybatisConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import com.github.pagehelper.PageHelper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+@MapperScan("com.scd.*.mapper")
+public class MybatisConfig {
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        //配置mysql数据库的方言
+        properties.setProperty("dialect","mysql");
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+}
