4e7ef59db6318391be7e8fc0d4dd8b464685a788
pupilman <542846492@qq.com>
2020-09-14 22:54:20 +0800

    集成 修补业务逻辑

--- yellow-tomatoes-system/src/main/java/com/scd/modules/system/service/impl/SysUserServiceImpl.java
+++ yellow-tomatoes-system/src/main/java/com/scd/modules/system/service/impl/SysUserServiceImpl.java
@@ -1,13 +1,12 @@
 package com.scd.modules.system.service.impl;
 
-import cn.hutool.db.PageResult;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.scd.common.service.impl.BaseServiceImpl;
+import com.scd.dozer.service.IGenerator;
 import com.scd.modules.system.domain.User;
+import com.scd.modules.system.service.DeptService;
+import com.scd.modules.system.service.JobService;
 import com.scd.modules.system.service.UserService;
 import com.scd.modules.system.service.dto.UserDto;
 import com.scd.modules.system.service.mapper.SysUserMapper;
@@ -16,24 +15,27 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
 @Service
 @CacheConfig(cacheNames = "user")
 public class SysUserServiceImpl extends BaseServiceImpl<SysUserMapper, User> implements UserService{
 
-//    private final IGenerator generator;
+    @Autowired
+    private IGenerator generator;
 
     @Autowired
     private SysUserMapper userMapper;
 
+    @Autowired
+    private JobService jobService;
+
+    @Autowired
+    private DeptService deptService;
+
     @Cacheable
     @Override
     public IPage<User> selectPageVo() {
-        System.out.println("scd");
         IPage page = new Page<User>(1, 2);
         List<User> users = userMapper.selectPageVo(page);
         page.setRecords(users);
@@ -51,12 +53,10 @@ public class SysUserServiceImpl extends BaseServiceImpl<SysUserMapper, User> imp
     public UserDto findByName(String userName) {
         User user =  userMapper.findByName(userName);
         //用户所属岗位
-//        user.setJob(jobService.getById(user.getJobId()));
+        user.setJob(jobService.getById(user.getJobId()));
         //用户所属部门
-//        user.setDept(deptService.getById(user.getDeptId()));
-//        return generator.convert(user,UserDto.class);
-        UserDto userDto = new UserDto();
-        return userDto;
+        user.setDept(deptService.getById(user.getDeptId()));
+        return generator.convert(user,UserDto.class);
     }
 
 }
